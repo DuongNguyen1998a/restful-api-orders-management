68e0ea2d7593c28dfdcbb9340b0a8f9954c90078
Duong Nguyen <duongnguyen03111998@gmail.com>
2021-03-03 23:04:47 +0700

    bla bla

--- back-end/routes/customers.js
+++ back-end/routes/customers.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const customerService = require('../services/customers_service');
 
-
 // Get All Customers
 router.get('/', async (req, res) => {
     await customerService.getCustomers(res);
